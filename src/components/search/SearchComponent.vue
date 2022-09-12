217a8b784f009bbebb12b82a2db9467b5542a883
Junior Alves <jrwanderley17@gmail.com>
2022-09-11 22:43:23 -0300

    feat: add SearchComponent

---
+++ src/components/search/SearchComponent.vue
@@ -0,0 +1,37 @@
+<template>
+<v-container>
+  <v-text-field
+    v-model="searchData"
+    append-icon="mdi-magnify"
+    :label="label"
+    single-line
+    hide-details
+    @input="search"
+  ></v-text-field>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'SearchComponent',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    searchData: ''
+  }),
+
+  methods: {
+    search() {
+      this.$emit('search', this.searchData)
+    }
+  }
+
+};
+</script>
+
+<style>
+</style>
