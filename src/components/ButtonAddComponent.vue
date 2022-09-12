973b23266da20d94a867800be0a1e9461db94ece
Junior Alves <jrwanderley17@gmail.com>
2022-09-11 22:42:32 -0300

    feat: add ButtonAddComponent

---
+++ src/components/ButtonAddComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-btn small @click="add" fab dark color="indigo">
+      <v-icon dark> mdi-plus </v-icon>
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ButtonAddComponent",
+  methods: {
+    add() {
+      this.$emit("add");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
